7d47cfc987824feec52abe5e7002e32b239fed06
ecommarketing <165447496+ecommarketing@users.noreply.github.com>
2024-04-24 20:56:06 +0530

    Update configuration.js

--- assets/theme/js/configuration.js
+++ assets/theme/js/configuration.js
@@ -8,7 +8,7 @@ var services = [
         "name": "Shopify Store",
         "icon": "fa-brands fa-shopify eicon",
         "description": "The platform is designed to provide a seamless and user-friendly experience for both store owners and customers.",
-        "price": "4999",
+        "price": "11999",
         "color_class": "shopify-wrapper",
         "animation": "assets/animations/shopify-animation.json",
         "features": ["Store Creation with unlimited Pages.", "Premium theme for store.", "50 Products Upload.", "Payment Gateway Setup."]
@@ -17,7 +17,7 @@ var services = [
         "name": "Meta Ads",
         "icon": "fa-brands fa-meta eicon",
         "description": "A powerful and widely used advertising tool that allows businesses and individuals to promote their products, services, or content on the world wide.",
-        "price": "6999",
+        "price": "19999",
         "color_class": "meta-wrapper",
         "animation": "assets/animations/meta-ads-animation.json",
         "features": ["Ads management.", "High Quality Creatives for Ads.", "Running multiple Effective and quality campaigns strategically.", "Optimization of campaigns and targeting the right audience andgenerating quality leads/ sales."]
@@ -26,7 +26,7 @@ var services = [
         "name": "Wordpress Store",
         "icon": "fa-brands fa-wordpress eicon",
         "description": "WordPress stores provide a wide range of possibilities for entrepreneurs, small businesses, and even large enterprises to sell products or services online.",
-        "price": "9999",
+        "price": "19999",
         "color_class": "wordpress-wrapper",
         "animation": "assets/animations/wordpress-animation.json",
         "features": ["Free Hosting with Domain for 1 year.", "Ecommerce website ,Multivendor website,Static website.", "Premium Theme.", "Products Upload 50.", "Daily Backup."]
@@ -35,7 +35,7 @@ var services = [
         "name": "Dropshipping",
         "icon": "fa-brands fa-dropbox eicon",
         "description": "When a store sells a product using the dropshipping model, it purchases the item from a third party and has it shipped directly to the customer.",
-        "price": "7999",
+        "price": "13999",
         "color_class": "dropship-wrapper",
         "animation": "assets/animations/dropshipping-animation.json",
         "features": ["Store Creation with connectivity to Roposo Dropshipping.", "High Quality Creatives for Ads.", "Running multiple Effective and quality campaigns strategically with 50 Products", "Optimization of campaigns and targeting the right audience andgenerating quality sales."]
@@ -44,7 +44,7 @@ var services = [
         "name": "International Dropshipping",
         "icon": "fa-solid fa-earth-asia eicon",
         "description": "Sourcing products from suppliers located in different countries, allowing merchants to reach customers across the globe.",
-        "price": "9999",
+        "price": "29999",
         "color_class": "international-dropship-wrapper",
         "animation": "assets/animations/international-dropshipping-animation.json",
         "features": ["Store Creation with connectivity to CJ Dropshipping.", "High Quality Creatives for Ads.", "Running multiple Effective and quality campaigns strategically with 50 Products", "Optimization of campaigns and targeting the right audience andgenerating quality sales."]
@@ -53,7 +53,7 @@ var services = [
         "name": "Google Ads",
         "icon": "fa-brands fa-google eicon",
         "description": "It allows businesses, advertisers, and individuals to display their ads on Google's search engine results pages (SERPs),websites and apps.",
-        "price": "9999",
+        "price": "24999",
         "color_class": "google-wrapper",
         "animation": "assets/animations/google-ads-animation.json",
         "features": ["Ads management.", "High Quality Creatives for Ads.", "Running multiple Effective and quality campaigns strategically.", "Optimization of campaigns and targeting the right audience andgenerating quality leads/ sales."]
@@ -62,7 +62,7 @@ var services = [
         "name": "Social Media Marketing",
         "icon": "fa-solid fa-hashtag eicon",
         "description": "Social media marketing is a dynamic and influential component of the modern digital marketing landscape.",
-        "price": "6999",
+        "price": "14999",
         "color_class": "social-media-marketing-wrapper",
         "animation": "assets/animations/social-media-marketing-animation.json",
         "features": ["Advertising", "Content Creation", "Profile optimization", "Audience Targeting", "Analytics and Insights", "Community Engagement"]
@@ -71,7 +71,7 @@ var services = [
         "name": "Affiliate Marketing",
         "icon": "fa-solid fa-link eicon",
         "description": "It is a process where publishers earn a commission by using an affiliate link to promote a product or service made by another retailer or advertiser.",
-        "price": "4999",
+        "price": "9999",
         "color_class": "affiliate-marketing-wrapper",
         "animation": "assets/animations/affiliate-marketing-animation.json",
         "features": ["Refersion", "Tapfiliate", "Action Plan", "Website Audit", "Number of Keywords Researched 22", "Number of Competitors Researched 6"]
@@ -89,7 +89,7 @@ var services = [
         "name": "Product Listing",
         "icon": "fa-solid fa-list eicon",
         "description": "Listing your products on various market-place like Amazon, Flipkart and meesho without facing any plenty.",
-        "price": "4999",
+        "price": "9999",
         "color_class": "product-listing-wrapper",
         "animation": "assets/animations/product-listing-animation.json",
         "features": ["Meta Description", "Title Optimization", "Number of Products 50", "Number of Words Per Description 180", "Number of Meta Tags / Keywords of Optimized 3"]
